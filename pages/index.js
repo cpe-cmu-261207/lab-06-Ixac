a8ba306838095ee634fa8810859099644d612c6b
Ixac <ixacarrrows@gmail.com>
2022-08-14 13:53:20 +0700

    ff

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,6 @@
-import {
-  IconChevronDown,
-  IconChevronUp,
-  IconMailForward,
-  IconMapPins,
-} from "@tabler/icons";
 import { useDebugValue, useState } from "react";
 import UserCard from "../Component/UserCard";
 import axios from "axios";
-import UserCardDetail from "../Component/UserCardDetail";
 
 export default function Home() {
   const [genAmount, setGenAmount] = useState(1);
