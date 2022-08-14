e2cc5c5706d76d7aa8f813edd38e6bd84555be5d
Ixac <ixacarrrows@gmail.com>
2022-08-14 17:49:20 +0700

    ee

--- component/UserCard.jsx
+++ component/UserCard.jsx
@@ -1,34 +1,31 @@
 import {
   IconChevronDown,
   IconChevronUp,
-  Iconmailforward,
+  IconMailForward,
   IconMapPins,
 } from "@tabler/icons";
 import React, { useState } from "react";
 import UserCardDetail from "./UserCardDetail";
 
 export default function UserCard(props) {
-  const [raiwa, setRaiwa] = useState(false);
+  const [isTap, setIsTap] = useState(false);
   const onClickHandler = () => {
-    raiwa === false ? setRaiwa(true) : setRaiwa(false);
+    {
+      isTap === false ? setIsTap(true) : setIsTap(false);
+    }
   };
-
   return (
     <div>
       <div className="border-bottom">
-        {/* {main section} */}
+        {/* main section */}
         <div className="d-flex align-items-center p-3" onClick={onClickHandler}>
-          <img
-            src={props.imgUrl}
-            width="90px"
-            className="rounded-circle me-4"
-          />
+          <img src={props.imgUrl} width="90px" class="rounded-circle me-4" />
           <span className="text-center display-6 me-auto">{props.name}</span>
-          {raiwa === false ? <IconChevronDown /> : <IconChevronUp />}
+          {isTap === false ? <IconChevronDown /> : <IconChevronUp />}
         </div>
       </div>
       {}
-      {raiwa === false ? null : (
+      {isTap === false ? null : (
         <UserCardDetail email={props.email} address={props.address} />
       )}
     </div>
